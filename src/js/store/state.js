1dd5a62636ccb36bbb06bc9d163c1b47e80420b4
hitari <hitari@naver.com>
2021-06-13 12:28:35 +0900

    mock 데이터 추가

--- src/js/store/state.js
+++ src/js/store/state.js
@@ -1,33 +1,9 @@
 export default {
   title: '이상형 월드컵',
   current: 0,
-  round: 8,
-  tournament: [
-    { url: '../assets/images/후라이드.jpg', name: '후라이드', isWinner: false },
-    { url: '../assets/images/양념.jpg', name: '양념', isWinner: false },
-    { url: '../assets/images/피자.jpg', name: '피자', isWinner: false },
-    { url: '../assets/images/떡볶이.jpg', name: '떡볶이', isWinner: false },
-    { url: '../assets/images/오일파스타.jpg', name: '오일파스타', isWinner: false },
-    { url: '../assets/images/크림파스타.jpg', name: '크림파스타', isWinner: false },
-    { url: '../assets/images/토마토파스타.jpg', name: '토마토파스타', isWinner: false },
-    { url: '../assets/images/로제파스타.jpg', name: '로제파스타', isWinner: false },
-  ],
+  round: 0,
+  tournament: [],
   winners: [],
   // ready, play, end
   status: 'ready',
 };
-
-const mock = [
-  { url: '../assets/images/후라이드.jpg', name: '후라이드', isWinner: false },
-  { url: '../assets/images/양념.jpg', name: '양념', isWinner: false },
-  { url: '../assets/images/피자.jpg', name: '피자', isWinner: false },
-  { url: '../assets/images/떡볶이.jpg', name: '떡볶이', isWinner: false },
-  { url: '../assets/images/오일파스타.jpg', name: '오일파스타', isWinner: false },
-  { url: '../assets/images/크림파스타.jpg', name: '크림파스타', isWinner: false },
-  { url: '../assets/images/토마토파스타.jpg', name: '토마토파스타', isWinner: false },
-  { url: '../assets/images/로제파스타.jpg', name: '로제파스타', isWinner: false },
-  { url: '../assets/images/샌드위치.jpg', name: '샌드위치', isWinner: false },
-  { url: '../assets/images/감자탕.jpg', name: '감자탕', isWinner: false },
-  { url: '../assets/images/삼계탕.jpg', name: '삼계탕', isWinner: false },
-  { url: '../assets/images/부대찌개.jpg', name: '부대찌개', isWinner: false },
-];
---
+++ src/mock/idealTypeMock.js
@@ -0,0 +1,20 @@
+const idealTypeMock = [
+  { url: '../assets/images/후라이드.jpg', name: '후라이드', isWinner: false },
+  { url: '../assets/images/양념.jpg', name: '양념', isWinner: false },
+  { url: '../assets/images/피자.jpg', name: '피자', isWinner: false },
+  { url: '../assets/images/떡볶이.jpg', name: '떡볶이', isWinner: false },
+  { url: '../assets/images/오일파스타.jpg', name: '오일파스타', isWinner: false },
+  { url: '../assets/images/크림파스타.jpg', name: '크림파스타', isWinner: false },
+  { url: '../assets/images/토마토파스타.jpg', name: '토마토파스타', isWinner: false },
+  { url: '../assets/images/로제파스타.jpg', name: '로제파스타', isWinner: false },
+  { url: '../assets/images/샌드위치.jpg', name: '샌드위치', isWinner: false },
+  { url: '../assets/images/감자탕.jpg', name: '감자탕', isWinner: false },
+  { url: '../assets/images/삼계탕.jpg', name: '삼계탕', isWinner: false },
+  { url: '../assets/images/부대찌개.jpg', name: '부대찌개', isWinner: false },
+  { url: '../assets/images/삼겹살.jpg', name: '삼겹살', isWinner: false },
+  { url: '../assets/images/짜장면.jpg', name: '짜장면', isWinner: false },
+  { url: '../assets/images/곱창구이.jpg', name: '곱창구이', isWinner: false },
+  { url: '../assets/images/스테이크.jpg', name: '스테이크', isWinner: false },
+];
+
+export default idealTypeMock;
